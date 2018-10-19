c70265ff99637c62f484b0e4aa0a9f70a435b69f
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-20 00:20:44 +0200

    add rwd to header for tablet

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//burger
+var nav = document.querySelector('nav');
+document.querySelector('.burger').addEventListener('click', function() {
+	this.classList.toggle('active');
+	nav.classList.toggle('active');	
+});
