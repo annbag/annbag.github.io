c3a77315d88fcb7a0f5d109711d8d9246a2ff06b
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-21 22:39:21 +0200

    add jQ to btn and fix nav

--- js/script.js
+++ js/script.js
@@ -4,6 +4,31 @@ document.querySelector('.burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 	nav.classList.toggle('active');	
 });
+$('.btn1').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('header').offset().top
+	}, 500)
+})
+$('.btn2').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.about').offset().top
+	}, 500)
+})
+$('.btn3').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.skills').offset().top
+	}, 750)
+})
+$('.btn4').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.portfolio').offset().top
+	}, 1000)
+})
+$('.btn5').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.contact').offset().top
+	}, 1250)
+})	
 
 $('a.arrow-down').click(function() {
 	$('html,body').animate({
@@ -31,3 +56,4 @@ $(document).ready(function() {
 		});
 	});
 });
+
