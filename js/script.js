cee59730b9b1555117f93f2bae6a40757594932d
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-21 20:12:34 +0200

    add jQ to arrow down

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 //burger
-var nav = document.querySelector('nav');
+const nav = document.querySelector('nav');
 document.querySelector('.burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 	nav.classList.toggle('active');	
+});
+
+$('a.arrow-down').click(function() {
+	$('html,body').animate({
+		scrollTop: $('.about').offset().top
+	}, 500)
 });
